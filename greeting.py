76b318ac4332cf3ea640258ddef61459db0118d9
tkmn1015 <s1F102302696@iniad.org>
2024-06-20 14:59:41 +0900

    Update greeting.py

--- greeting.py
+++ greeting.py
@@ -1,6 +1,15 @@
-def greet(name):
-    message = 'Hello, ' + name + '-san!'
+
+from datetime import datetime
+
+def greet():
+    hour = datetime.now().hour
+    if hour <= 11:
+        message = 'Good morning'
+    elif hour <= 17:
+        message = 'Hello'
+    else:
+        message = 'Good evening'
     print(message)
 
 
-greet('Inoue')
+greet()
